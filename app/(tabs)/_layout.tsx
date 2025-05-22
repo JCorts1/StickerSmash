6c563f4d24fc37d4ed4b8691f406cb4777516603
Jay Cortes <cortesjay1991@gmail.com>
2025-05-22 16:09:02 +0200

    Routes are done, tabs are done

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,34 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+    screenOptions={{
+      tabBarActiveTintColor: "ffd33d",
+      headerStyle: {
+        backgroundColor: "#25292e"
+      },
+      headerShadowVisible: false,
+      headerTintColor: "#fff",
+      tabBarStyle: {
+        backgroundColor: "25292e",
+      },
+    }}
+    >
+      <Tabs.Screen
+      name="index"
+      options={{
+        headerTitle: "StickerSmash",
+        tabBarIcon: ({focused, color}) =>
+          <Ionicons
+            name={focused ?  "home-sharp" : "home-outline"}
+            size={30}
+            color = {color}
+            />
+        }} />
+      <Tabs.Screen name="about" options={{ headerTitle: "About" }} />
+    </Tabs>
+  )
+}
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,9 @@
-import { Link } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>About</Link>
     </View>
   );
 }
